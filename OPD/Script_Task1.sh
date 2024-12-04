0c14cacefe54acb024b4b575df9b453358d10ba9
kaysiodl <125454782+kaysiodl@users.noreply.github.com>
2024-12-04 23:16:36 +0300

    Add files via upload

---
+++ OPD/Script_Task1.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+
+mkdir lab0
+cd lab0
+
+#Task 1
+mkdir alakazam4
+cd alakazam4
+touch shuppet
+touch pansage
+touch munna
+mkdir swampert
+echo "Возможности  Overland=1 Surface=4 Sky=4 Jump=3 Power1=0
+Intelligence=4 Invisibility=0 Phasing=0 Stealth=0" > shuppet
+echo "Тип диеты
+Omnivore" > pansage
+echo "Тип диеты  Herbivore" > munna
+cd ..
+
+touch beheeyem6
+touch horsea6
+echo "Тип диеты
+Nullivore" > beheeyem6
+echo "weight=17.6 height=16.0 atk=4 def=7" > horsea6
+
+mkdir murkrow6
+cd murkrow6
+mkdir swampert
+touch lillipup
+touch aron
+echo "Тип
+диеты  Omnivore" > lillipup
+echo "Способности   Tackle Harden Mud-Slap Headbutt
+Metal Claw Iron Defense Roar Take Down Iron Head Protect Metal Sound
+Iron Tail Automize Heavy Slam Double-Edge Metal Burst" > aron
+cd ..
+
+mkdir seadra6
+cd seadra6 
+mkdir kabuto
+mkdir phanpy
+touch wurmple
+echo "Тип
+диеты  Herbivore" > wurmple
+cd ..
+
+touch ursaring2
+echo "weight=277.3 height=71.0 atk=13 def=8" > ursaring2
---
+++ OPD/Script_Task2.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+cd lab0
+
+#Task 2
+chmod u=wx,g=rwx,o=rx alakazam4
+chmod ug=, o=rw alakazam4/shuppet
+chmod ugo=r alakazam4/pansage
+chmod ug=,o=rw alakazam4/munna
+chmod u=wx,go=x alakazam4/swampert
+
+chmod u=rw,g=,o=r beheeyem6
+chmod 066 horsea6
+
+chmod 335 murkrow6
+chmod u=rwx,g=wx,o=u-x murkrow6/swampert
+chmod 624 murkrow6/lillipup
+chmod 066 murkrow6/aron
+
+chmod 736 seadra6
+chmod 307 seadra6/kabuto
+chmod 550 seadra6/phanpy
+chmod uo=,g=rw seadra6/wurmple
+
+chmod 004 ursaring2
+
+
+
+
+
